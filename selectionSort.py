f3fb17a8329761a93e5433c1c6ed5452a4b06ff8
sashazhang <sasha_zhang@aw.org>
2021-10-21 22:20:21 -0700

    21/10/21

---
+++ selectionSort.py
@@ -0,0 +1,32 @@
+def sort():
+    numList = [2,3,30,8,5]
+    print(numList)
+
+    minimum = numList[0]
+    x = 0
+
+
+    
+    for a in range(len(numList)-1):
+        minimum = numList[a]
+        x = a
+        for i in range(a+1,len(numList)):
+            #print(numList[a])
+            if (numList[i]) < minimum:
+                minimum = (numList[i])
+                x = i
+                #print(minimum)
+        temp = numList[a]
+        numList[a] = numList[x]
+        numList[x] = temp
+
+    return numList
+
+def main():
+
+    print(sort())
+    
+
+
+if __name__ == "__main__":
+    main()
