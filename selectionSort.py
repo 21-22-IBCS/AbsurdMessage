ddc6da981ff52210a14e1f47f6ea5e4acf0b9b04
sashazhang <sasha_zhang@aw.org>
2021-10-22 13:42:37 -0700

    😩

--- selectionSort.py
+++ selectionSort.py
@@ -10,12 +10,14 @@ def sort():
     for a in range(len(numList)-1):
         minimum = numList[a]
         x = a
+        #check the next number each time
         for i in range(a+1,len(numList)):
             #print(numList[a])
             if (numList[i]) < minimum:
                 minimum = (numList[i])
                 x = i
                 #print(minimum)
+        #to swap
         temp = numList[a]
         numList[a] = numList[x]
         numList[x] = temp
